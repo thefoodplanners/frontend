1cd15857f4d2305da2d48e5241ae777b1ac857c6
Ethan Hullett <ethull@protonmail.com>
2022-11-11 18:02:22 +0000

    add layout

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,23 +1,9 @@
-import { Outlet, Link } from "react-router-dom";
+import NavBar from "../components/navbar"
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
+      <NavBar />
     </>
   )
 };
