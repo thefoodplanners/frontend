0cdf80028eaaf4bdb4d942760942d2a1fc7cce3a
Ethan Hullett <ethull@protonmail.com>
2022-11-11 21:55:16 +0000

    add footer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './scss/custom.scss';
-
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Container } from "react-bootstrap"
 
 import Header from "./components/header";
 import Home from "./pages/home";
 import Dashboard from "./pages/dashboard";
 import Login from "./pages/login";
+import Register from "./pages/register";
 import NoPage from "./pages/nopage";
-
+import Footer from "./components/footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
+      <Container className="mb-4">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </Container>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap"
+
+export default function Footer(){
+    return (
+      <footer className="footer mt-auto py-3 bg-light">
+        <Container>
+          <p> *Copyright thefoodplanners, we are awesome </p>
+        </Container>
+      </footer>
+    )
+}
