fbb60864b08621be7e53568c74fd966481eb7557
Ethan Hullett <ethull@protonmail.com>
2023-01-19 14:59:35 +0000

    dashboard placeholder

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,5 +1,13 @@
+import Image from 'react-bootstrap/Image'
+import MealPlannerImage from '../assets/meal_planner_image.png'
+
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Image src={MealPlannerImage} />
+    </div>
+  );
 };
 
 export default Dashboard;
