82144a94807a9fe0c658b6a499f83ee3ba65ce16
Ethan Hullett <ethull@protonmail.com>
2023-01-19 14:27:05 +0000

    carousel

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,56 @@
+import Carousel from 'react-bootstrap/Carousel';
+//import MealPlannerImage from '../assets/meal_planner_image.png';
+import SarabRunning1 from '../assets/sarab_running_1.jpeg';
+import SarabRunning2 from '../assets/sarab_running_2.jpeg';
+import SarabRunning3 from '../assets/sarab_running_3.jpeg';
+import Image from 'react-bootstrap/Image';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <Image 
+            className="d-block w-100"
+            src={SarabRunning1} 
+            style={{maxHeight: "50vh", backgroundSize: "cover"}}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image 
+            className="d-block w-100 100px"
+            src={SarabRunning2} 
+            style={{maxHeight: "50vh"}}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image 
+            className="d-block w-100 h-50"
+            src={SarabRunning3} 
+            style={{maxHeight: "50vh"}}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+
 };
 
 export default Home;
