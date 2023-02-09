f4709326600b3621d51482338d22c3d64f47ade2
Ethan Hullett <ethull@protonmail.com>
2023-02-09 17:25:52 +0000

    display backend data on calender

--- src/components/calender.jsx
+++ src/components/calender.jsx
@@ -3,12 +3,14 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
+import Spinner from 'react-bootstrap/Spinner';
 
 const Calender = () => {
   //const [currentWeek, setCurrentWeek] = useState(new Date());
   //const [selectedDay, setSelectedDay] = useState();
   // meals by day
   const [currentWeekMeals, setCurrentWeekMeals] = useState([]);
+  const [selectedDate, setSelectedDate] = useState();
   
   // x index = day, y index = meal number
   useEffect(() => {
@@ -21,17 +23,21 @@ const Calender = () => {
       credentials: 'include',
     }).then((response) => {
       console.log(response);
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      setCurrentWeekMeals(data);
     });
-    setCurrentWeekMeals(
-      [
-        [{name: "eggs", id: 69},{name: "chicken", id: 27}], // monday
-        [{name: "m1"},{name: "m2"},{name: "m3"}], // tuesday
-        [], // wednesday
-        [{name: "m1"},{name: "m2"},{name: "m3"},{name: "m4"},{name: "m5"},{name: "m6"}], // thursday
-        [], // friday
-        [], // saterday
-        [], // sunday
-      ]);
+    //setCurrentWeekMeals(
+    //  [
+    //    [{name: "eggs", id: 69},{name: "chicken", id: 27}], // monday
+    //    [{name: "m1"},{name: "m2"},{name: "m3"}], // tuesday
+    //    [], // wednesday
+    //    [{name: "m1"},{name: "m2"},{name: "m3"},{name: "m4"},{name: "m5"},{name: "m6"}], // thursday
+    //    [], // friday
+    //    [], // saterday
+    //    [], // sunday
+    //  ]);
   }, []);
   
   // render the button used to move week in the calender
@@ -66,39 +72,45 @@ const Calender = () => {
     )
   }
 
-  const renderParentRows = () => (
-    <Row className="gx-0">
-      <Col xs="3"></Col>
-      <Col xs="1" className="text-center border">  
-        Mon
-        {currentWeekMeals.length !== 0 && renderDayRow(0)}
-      </Col>
-      <Col xs="1" className="text-center border"> 
-        Tues 
-        {currentWeekMeals.length !== 0 && renderDayRow(1)}
-      </Col>
-      <Col xs="1" className="text-center border"> 
-        Wed 
-        {currentWeekMeals.length !== 0 && renderDayRow(2)}
-      </Col>
-      <Col xs="1" className="text-center border"> 
-        Thurs 
-        {currentWeekMeals.length !== 0 && renderDayRow(3)}
-      </Col>
-      <Col xs="1" className="text-center border"> 
-        Fri 
-        {currentWeekMeals.length !== 0 && renderDayRow(4)}
-      </Col>
-      <Col xs="1" className="text-center border"> 
-        Sat 
-        {currentWeekMeals.length !== 0 && renderDayRow(5)}
-      </Col>
-      <Col xs="1" className="text-center border"> 
-        Sun 
-        {currentWeekMeals.length !== 0 && renderDayRow(6)}
-      </Col>
-    </Row>
-  )
+  const renderParentRows = () => {
+    if (currentWeekMeals.length === 0)
+      return <Spinner></Spinner>;
+    else
+      return (
+        <Row className="gx-0">
+          <Col xs="3"></Col>
+          <Col xs="1" className="text-center border">  
+            Mon
+            {renderDayRow(0)}
+          </Col>
+          <Col xs="1" className="text-center border"> 
+            Tues 
+            {renderDayRow(1)}
+          </Col>
+          <Col xs="1" className="text-center border"> 
+            Wed 
+            {renderDayRow(2)}
+          </Col>
+          <Col xs="1" className="text-center border"> 
+            Thurs 
+            {renderDayRow(3)}
+          </Col>
+          <Col xs="1" className="text-center border"> 
+            Fri 
+            {renderDayRow(4)}
+          </Col>
+          <Col xs="1" className="text-center border"> 
+            Sat 
+            {renderDayRow(5)}
+          </Col>
+          <Col xs="1" className="text-center border"> 
+            Sun 
+            {renderDayRow(6)}
+            {/* currentWeekMeals.length !== 0 && renderDayRow(6) */}
+          </Col>
+        </Row>
+      )
+  }
   
   //const renderRow = () => {
   //  console.log("renderrows");
