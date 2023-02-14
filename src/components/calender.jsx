3c3b2cbd6f1a2c96e6e1fc3576a8d782fc6b8c31
Ethan Hullett <ethull@protonmail.com>
2023-02-14 01:28:53 +0000

    ui updates

--- src/components/calender.jsx
+++ src/components/calender.jsx
@@ -6,6 +6,7 @@ import Stack from 'react-bootstrap/Stack';
 import Spinner from 'react-bootstrap/Spinner';
 import Image from 'react-bootstrap/Image';
 import SarabRunning1 from '../assets/sarab_running_1.jpeg';
+import Button from 'react-bootstrap/Button';
 
 const Calender = () => {
   //const [currentWeek, setCurrentWeek] = useState(new Date());
@@ -44,16 +45,16 @@ const Calender = () => {
   
   // render the button used to move week in the calender
   const renderWeekChanger = () => (
-      <Row gap="0">
-        <Col xs="1"></Col>
-        <Col xs="2">
-          <Stack direction="horizontal" gap={1}>
-            <i className="bi bi-arrow-left-circle border"></i>
-            <div className="border">Week</div>
-            <i className="bi bi-arrow-right-circle border"></i>
+      <div className="row gx-0 seven-cols">
+        {/* <div className="col-xs-0 col-md-1"></div> */}
+        <div className="col-xs-12 col-md-1">
+          <Stack direction="horizontal" gap={3}>
+            <i className="bi bi-arrow-left-circle"></i>
+            <div className="">Week</div>
+            <i className="bi bi-arrow-right-circle"></i>
           </Stack>
-        </Col>
-      </Row>
+        </div>
+      </div>
   ); 
 
   const renderItem = (item,index) => {
@@ -61,7 +62,7 @@ const Calender = () => {
       <div>
         <div> {item.name} </div>
         <div> {item.mealType} </div>
-        <div> {item.calories} </div>
+        <div> {item.calories} cals </div>
         <Image 
           className="d-block w-100"
           src={SarabRunning1} 
@@ -82,13 +83,23 @@ const Calender = () => {
           style={{minHeight: "50vh"}}
         >
           {
+            currentWeekMeals[day].length > 0 &&
             currentWeekMeals[day].map((item,index)=>(
-              <div xs="1" key={index} className="text-center border border-1 w-100 h-100 flex-grow-1 bg-white"> {renderItem(item,index)} </div>
+              <div key={index} className="text-center border border-1 w-100 h-100 flex-grow-1 bg-white d-flex justify-content-center align-items-center"> {renderItem(item,index)} </div>
             ))
+          }
+
+          {
+            currentWeekMeals[day].length === 0 &&
+              <div className="text-center border border-1 w-100 h-100 flex-grow-1 bg-white d-flex justify-content-center align-items-center"> <i class="bi bi-plus"></i> </div>
           }
         </div>
       </div>
     )
+  }
+  
+  const getTotalDayCalories = (day) => {
+    return currentWeekMeals[day].reduce((sum, item) => sum + item.calories, 0);
   }
 
   const renderParentRows = () => {
@@ -96,34 +107,41 @@ const Calender = () => {
       return <Spinner></Spinner>;
     else
       return (
-        <div className="row gx-0 shadow-lg seven-cols">
-          <div xs="1" className="col text-center">  
+        <div className="row gx-0 seven-cols">
+          <div className="col-xs-12 col-md-1 text-center">  
             <div className="bg-primary"> Monday </div>
             {renderDayRow(0)}
+            <div className=""> {getTotalDayCalories(0)} cals </div>
           </div>
-          <div xs="1" className="col text-center"> 
+          <div className="col-xs-12 col-md-1 text-center"> 
             <div className="bg-primary"> Tuesday </div>
             {renderDayRow(1)}
+            <div className=""> {getTotalDayCalories(1)} cals </div>
           </div>
-          <div xs="1" className="col text-center"> 
+          <div className="col-xs-12 col-md-1 text-center"> 
             <div className="bg-primary"> Wednesday </div>
             {renderDayRow(2)}
+            <div className=""> {getTotalDayCalories(2)} cals </div>
           </div>
-          <div xs="1" className="col text-center"> 
+          <div className="col-xs-12 col-md-1 text-center"> 
             <div className="bg-primary"> Thursday </div>
             {renderDayRow(3)}
+            <div className=""> {getTotalDayCalories(3)} cals </div>
           </div>
-          <div xs="1" className="col text-center"> 
+          <div className="col-xs-12 col-md-1 text-center"> 
             <div className="bg-primary"> Friday </div>
             {renderDayRow(4)}
+            <div className=""> {getTotalDayCalories(4)} cals </div>
           </div>
-          <div xs="1" className="col text-center"> 
+          <div className="col-xs-12 col-md-1 text-center"> 
             <div className="bg-primary"> Saterday </div>
             {renderDayRow(5)}
+            <div className=""> {getTotalDayCalories(5)} cals </div>
           </div>
-          <div xs="1" className="col text-center"> 
+          <div className="col-xs-12 col-md-1 text-center"> 
             <div className="bg-primary"> Sunday </div>
             {renderDayRow(6)}
+            <div className=""> {getTotalDayCalories(6)} cals </div>
             {/* currentWeekMeals.length !== 0 && renderDayRow(6) */}
           </div>
         </div>
@@ -138,6 +156,9 @@ const Calender = () => {
     <Container fluid>
       {renderWeekChanger()}
       {renderParentRows()}
+      <Button className="ms-auto btn" variant="primary">
+        Add a new meal
+      </Button>
     </Container>
   );
 };
