cc5e8f9cfa534540ef3532ebf28da4ffe917c428
Ethan Hullett <ethull@protonmail.com>
2023-03-01 17:03:44 +0000

    add date switching

--- src/components/calender.jsx
+++ src/components/calender.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 //import Row from 'react-bootstrap/Row';
 //import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import Stack from 'react-bootstrap/Stack';
 import Spinner from 'react-bootstrap/Spinner';
 import Image from 'react-bootstrap/Image';
 import SarabRunning1 from '../assets/sarab_running_1.jpeg';
@@ -16,12 +15,13 @@ const Calender = () => {
   // meals by day
   const [currentWeekMeals, setCurrentWeekMeals] = useState([]);
   const [fetchedMeals, setFetchedMeals] = useState(false);
-  //const [selectedDate, setSelectedDate] = useState();
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const [showSugestionModal, setShowSugestionModal] = useState(false);
   
   // x index = day, y index = meal number
   useEffect(() => {
-    fetch("http://localhost:9000/allMeals?weekDate=2023-01-30", {
+    //2023-02-30
+    fetch(`http://localhost:9000/allMeals?weekDate=${selectedDate.getFullYear()}-${selectedDate.getMonth()+1}-${selectedDate.getDate()}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -46,21 +46,33 @@ const Calender = () => {
     //    [], // saterday
     //    [], // sunday
     //  ]);
-  }, []);
+  }, [selectedDate]);
   
   // render the button used to move week in the calender
   const renderWeekChanger = () => (
-      <div className="row gx-0 seven-cols">
+      <div className="row gx-0 seven-cols mb-3 mt-3">
         {/* <div className="col-xs-0 col-md-1"></div> */}
-        <div className="col-xs-12 col-md-1">
-          <Stack direction="horizontal" gap={3}>
-            <i className="bi bi-arrow-left-circle"></i>
-            <div className="">Week</div>
-            <i className="bi bi-arrow-right-circle"></i>
-          </Stack>
+        <div className="col-xs-12 col-md-3">
+          <div className="d-flex justify-content-between">
+            <Button className="btn" onClick={() => updateDate(-7)}>
+              <i className="bi bi-arrow-left"></i>
+            </Button>
+            <div className="text-center"> { 
+              selectedDate.toDateString() 
+            } </div>
+            <Button className="btn" onClick={() => updateDate(7)}>
+              <i className="bi bi-arrow-right"></i>
+            </Button>
+          </div>
         </div>
       </div>
   ); 
+  
+  const updateDate = (value) => {
+    setFetchedMeals(false);
+    setCurrentWeekMeals([])
+    setSelectedDate(new Date(selectedDate.setDate(selectedDate.getDate() + value)))
+  }
 
   const renderItem = (item,index) => {
     return (
@@ -99,7 +111,7 @@ const Calender = () => {
           {
             // if we have fetched meals from the backend and we have no meals
             fetchedMeals && currentWeekMeals[day].length === 0 &&
-              <div className="text-center border border-1 w-100 h-100 flex-grow-1 bg-white shadow-sm d-flex justify-content-center align-items-center"> <i class="bi bi-plus"></i> </div>
+              <div className="text-center border border-1 w-100 h-100 flex-grow-1 bg-white shadow-sm d-flex justify-content-center align-items-center"> <i className="bi bi-plus fs-1"></i> </div>
           }
 
           {
@@ -194,7 +206,7 @@ const SugestionModal = (props) => {
       centered
       className="shadow-lg"
     >
-      <Modal.Header class="bg-primary" closeButton>
+      <Modal.Header className="bg-primary" closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
           Add a meal?
         </Modal.Title>
