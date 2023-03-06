43f02c33a243d09cde83175845650dabb115b842
Ethan Hullett <ethull@protonmail.com>
2023-03-06 23:59:02 +0000

    footer text

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
     return (
       <footer className="footer mt-auto py-3 bg-dark text-light">
         <Container>
-          <p className=""> *Copyright thefoodplanners, we are awesome </p>
+          <p className=""> *Copyright FoodGen.org (¬‿¬ ) </p>
         </Container>
       </footer>
     )
