bbb09849f4496b72226854a134e5070edb416487
Ethan Hullett <ethull@protonmail.com>
2023-03-06 23:59:32 +0000

    update suggeestion modal

--- src/components/calender.jsx
+++ src/components/calender.jsx
@@ -3,11 +3,9 @@ import { useState, useEffect } from 'react';
 //import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Spinner from 'react-bootstrap/Spinner';
-import Image from 'react-bootstrap/Image';
-import SarabRunning1 from '../assets/sarab_running_1.jpeg';
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import ListGroup from 'react-bootstrap/ListGroup';
+
+import SugestionModal from './suggestionModal';
 
 const Calender = () => {
   //const [currentWeek, setCurrentWeek] = useState(new Date());
@@ -92,21 +90,17 @@ const Calender = () => {
     const imageRef = `data:image/jpeg;base64,${item.imageRef}`;
     return (
       <div
-        style={{backgroundImage: "url('" + imageRef + "')",  backgroundSize: "cover", backgroundPosition: "center"}}
-        className="w-100 d-flex flex-column justify-content-center align-items-center"
+        style={{backgroundImage: "url('" + imageRef + "')",  backgroundSize: "cover", backgroundPosition: "center", display: "grid", gridTemplateColumns: "1fr"}}
+        className="w-100"
       >
-        <div className="text-white fw-bold" style={{textShadow: "0px 0px 10px  black", backgroundColor: "#0008", width: "fit-content", padding: "0px 4px"}}> {item.name} </div>
-        <div className="text-white fw-bold" style={{textShadow: "0px 0px 10px  black", backgroundColor: "#0008", width: "fit-content", padding: "0px 4px"}}> {item.mealType} </div>
-        <div className="text-white fw-bold" style={{textShadow: "0px 0px 10px  black", backgroundColor: "#0008", width: "fit-content", padding: "0px 4px"}}> {item.calories} cals </div>
-        {/*
-        <Image 
-          className="d-block w-100"
-          src={`data:image/jpeg;base64,${item.imageRef}`} 
-          style={{maxHeight: "1vh", backgroundSize: "cover"}}
-          alt="First slide"
-          roundedCircle
-        />
-      */}
+        <div className="w-100 d-flex justify-content-start align-items-right z-1" style={{gridRowStart: "1", gridColumnStart: "1", padding: "0px 4px"}}>
+          <i class="bi bi-x-lg"></i>
+        </div>
+        <div className="w-100 d-flex flex-column justify-content-center align-items-center" style={{gridRowStart: "1", gridColumnStart: "1"}}>
+          <div className="text-white fw-bold" style={{textShadow: "0px 0px 10px  black", backgroundColor: "#0008", width: "fit-content", padding: "0px 4px"}}> {item.name} </div>
+          <div className="text-white fw-bold" style={{textShadow: "0px 0px 10px  black", backgroundColor: "#0008", width: "fit-content", padding: "0px 4px"}}> {item.mealType} </div>
+          <div className="text-white fw-bold" style={{textShadow: "0px 0px 10px  black", backgroundColor: "#0008", width: "fit-content", padding: "0px 4px"}}> {item.calories} cals </div>
+        </div>
       </div>
     )
   }
@@ -238,100 +232,5 @@ const getTotalWeekCalories = () => {
     </Container>
   );
 };
-
-const SugestionModal = (props) => {
-  const [addMealList, setAddMealList] = useState([]);
-  const [selectedMealStates, setSelectedMealStates] = useState([false, false, false]);
-  const [currentSuggestionsIndex, setCurrentSuggestionsIndex] = useState(0);
-
-  // why does this keep running?
-  useEffect(() => {
-    console.log("use effect run: ");
-    //console.log(addMealList);
-    // fetch meals we can add
-    fetch("http://localhost:9000/recommendMeals", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      withCredentials: true,
-      credentials: 'include',
-    }).then((response) => {
-      //console.log(response);
-      return response.json();
-    }).then((data) => {
-      console.log(data);
-      setAddMealList(data);
-    });
-  }, [addMealList]);
-  
-  const handleItemSelected = (listGroupIndex) => {
-    const newSelectedMealStates = [...selectedMealStates];
-    setSelectedMealStates(newSelectedMealStates.map((item, index) => {
-      if (index === listGroupIndex) return true;
-      else return false;
-    }))
-  }
-  
-  const handleNextSuggestionsClick = (move) => {
-    // len = 3, index = 2, move= +1: index=(index+1)%len: index=2+1=3%3=0
-    // len = 3, index = 0, move= -1: index=(index+1)%len: index=2+1=3%3=0
-    // if index out of bound then wrap around
-    let nextIndex = currentSuggestionsIndex+move;
-    if (nextIndex === -1) nextIndex = addMealList.length-1
-    else if (nextIndex === addMealList.length) nextIndex = 0;
-    setCurrentSuggestionsIndex(nextIndex);
-    setSelectedMealStates([false, false, false])
-    //setSelectedMealStates(Array(data[0].length).fill(false))
-  }
-  
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      className="shadow-lg"
-    >
-      <Modal.Header className="bg-primary" closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Add a meal?
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Suggested meals</h4>
-        {
-          // if we have fetched meal list
-          addMealList.length !== 0 &&
-          <ListGroup as="ol" numbered>
-            {
-              addMealList[currentSuggestionsIndex].map((item,index) => (
-                <ListGroup.Item key={index} as="li" active={selectedMealStates[index]} onClick={() => handleItemSelected(index)}> {item.name} </ListGroup.Item>
-              ))
-            }
-          </ListGroup>
-        }
-        {
-          // if we have fetched meal list
-          addMealList.length === 0 &&
-          <Spinner 
-            animation="border" 
-            role="status"
-            style={{
-              maxWidth: "480px",
-            }}
-          />
-        }
-      <div id="button-container" className="mt-5 d-flex justify-content-between align-items-center">
-        <Button onClick={() => {handleNextSuggestionsClick(-1)}}>Previous suggestions</Button>
-        <Button onClick={() => {handleNextSuggestionsClick(1)}}>More suggestions</Button>
-      </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
 
 export default Calender;
---
+++ src/components/suggestionModal.jsx
@@ -0,0 +1,137 @@
+import { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Spinner from 'react-bootstrap/Spinner';
+
+const SugestionModal = (props) => {
+  const [addMealList, setAddMealList] = useState([]);
+  const [selectedMealStates, setSelectedMealStates] = useState([false, false, false]);
+  const [currentSuggestionsIndex, setCurrentSuggestionsIndex] = useState(0);
+
+  // TODO why does this keep running over and over?
+  useEffect(() => {
+    //console.log("use effect run: ");
+    //console.log(addMealList);
+    // fetch meals we can add
+    fetch("http://localhost:9000/recommendMeals", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+      credentials: 'include',
+    }).then((response) => {
+      //console.log(response);
+      return response.json();
+    }).then((data) => {
+      //console.log(data);
+      setAddMealList(data);
+    });
+  }, []);
+  //}, [addMealList]);
+  
+  const handleItemSelected = (listGroupIndex) => {
+    const newSelectedMealStates = [...selectedMealStates];
+    setSelectedMealStates(newSelectedMealStates.map((item, index) => {
+      if (index === listGroupIndex) return true;
+      else return false;
+    }))
+  }
+  
+  const handleNextSuggestionsClick = (move) => {
+    // len = 3, index = 2, move= +1: index=(index+1)%len: index=2+1=3%3=0
+    // len = 3, index = 0, move= -1: index=(index+1)%len: index=2+1=3%3=0
+    // if index out of bound then wrap around
+    let nextIndex = currentSuggestionsIndex+move;
+    if (nextIndex === -1) nextIndex = addMealList.length-1
+    else if (nextIndex === addMealList.length) nextIndex = 0;
+    setCurrentSuggestionsIndex(nextIndex);
+    setSelectedMealStates([false, false, false])
+    //setSelectedMealStates(Array(data[0].length).fill(false))
+  }
+
+  // add an item, can only be called when an item is selected
+  const handleAddItem = () => {
+    const currentSelectedIndex = selectedMealStates.indexOf(true);
+    const mealToAdd = addMealList[currentSuggestionsIndex][currentSelectedIndex];
+    console.log(mealToAdd);
+    props.onHide();
+    const currentDate = new Date();
+    fetch("http://localhost:9000/addMealSlot", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        date: `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`,
+        mealNum: 0,
+        recipeId: mealToAdd.id,
+      },
+      withCredentials: true,
+      credentials: 'include',
+    }).then((response) => {
+      //console.log(response);
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      // TODO add the item to table
+    });
+  }
+  
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      className="shadow-lg"
+    >
+      <Modal.Header className="bg-primary" closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add a meal?
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Suggested meals</h4>
+        {
+          // if we have fetched meal list
+          addMealList.length !== 0 &&
+          <ListGroup as="ol" numbered>
+            {
+              addMealList[currentSuggestionsIndex].map((item,index) => (
+                <ListGroup.Item key={index} as="li" active={selectedMealStates[index]} onClick={() => handleItemSelected(index)}> {item.name} </ListGroup.Item>
+              ))
+            }
+          </ListGroup>
+        }
+        {
+          // if we have fetched meal list
+          addMealList.length === 0 &&
+          <Spinner 
+            animation="border" 
+            role="status"
+            style={{
+              maxWidth: "480px",
+            }}
+          />
+        }
+      <div id="button-container" className="mt-5 d-flex justify-content-between align-items-center">
+        <Button onClick={() => {handleNextSuggestionsClick(-1)}}>Previous suggestions</Button>
+        <Button onClick={() => {handleNextSuggestionsClick(1)}}>More suggestions</Button>
+      </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button 
+          onClick={() => {handleAddItem()}}
+          disabled={!selectedMealStates.includes(true)}
+        > 
+          <i class="bi bi-plus fs-4"></i>
+        </Button>
+        {/* <Button onClick={props.onHide}>Close</Button> */}
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default SugestionModal;
