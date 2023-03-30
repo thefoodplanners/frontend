661bbca48e998e68969d512dca4a6f1d227b8460
Ethan Hullett <ethull@protonmail.com>
2023-03-30 21:04:14 +0100

    update search

--- src/components/suggestionModal.jsx
+++ src/components/suggestionModal.jsx
@@ -5,21 +5,17 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Spinner from 'react-bootstrap/Spinner';
 import AsyncSelect from 'react-select/async';
 
-const SuggestionModal = (props) => {
+// pane for dynamic suggestion items
+const SuggestionPane = (props) => {
   const [addMealList, setAddMealList] = useState([]);
   // state for suggestions
   const [selectedMealStates, setSelectedMealStates] = useState([false, false, false]);
   const [currentSuggestionsIndex, setCurrentSuggestionsIndex] = useState(0);
-  
-  // state for search bar
-  const [searchOptions, setSearchOptions] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
 
   // TODO why does this keep running over and over?
   // fetch options for suggested options
   useEffect(() => {
     //console.log("use effect run: ");
-    //console.log(addMealList);
     // fetch meals we can add
     fetch("http://localhost:9000/calendar/meals/recommendation", {
       method: "GET",
@@ -29,15 +25,77 @@ const SuggestionModal = (props) => {
       withCredentials: true,
       credentials: 'include',
     }).then((response) => {
-      //console.log(response);
       return response.json();
     }).then((data) => {
-      console.log(data);
+      //console.log(data);
       setAddMealList(data);
     });
   }, []);
   //}, [addMealList]);
 
+  const handleItemSelected = (listGroupIndex) => {
+    // set the meal as selected
+    //const newSelectedMealStates = [...selectedMealStates].map((item, index) => {
+    const newSelectedMealStates = selectedMealStates.map((item, index) => {
+      if (index === listGroupIndex) return true;
+      else return false;
+    });
+    setSelectedMealStates(newSelectedMealStates);
+    
+    const currentSelectedIndex = newSelectedMealStates.indexOf(true);
+    const mealToAdd = addMealList[currentSuggestionsIndex][currentSelectedIndex];
+    // set parent dialog to the selected meal
+    props.setMealToAdd(mealToAdd.id);
+  }
+  
+  const handleNextSuggestionsClick = (move) => {
+    // len = 3, index = 2, move= +1: index=(index+1)%len: index=2+1=3%3=0
+    // len = 3, index = 0, move= -1: index=(index+1)%len: index=2+1=3%3=0
+    // if index out of bound then wrap around
+    let nextIndex = currentSuggestionsIndex+move;
+    if (nextIndex === -1) nextIndex = addMealList.length-1
+    else if (nextIndex === addMealList.length) nextIndex = 0;
+    setCurrentSuggestionsIndex(nextIndex);
+    setSelectedMealStates([false, false, false])
+    props.setMealToAdd(-1);
+    //setSelectedMealStates(Array(data[0].length).fill(false))
+  }
+
+  return (
+    <div>
+      <h4>Suggested meals</h4>
+      {
+        // if we have fetched meal list
+        addMealList.length !== 0 &&
+        <ListGroup as="ol" numbered>
+          {
+            addMealList[currentSuggestionsIndex].map((item,index) => (
+              <ListGroup.Item key={index} as="li" active={selectedMealStates[index]} onClick={() => handleItemSelected(index)}> {item.name} </ListGroup.Item>
+            ))
+          }
+        </ListGroup>
+      }
+      {
+        // if we have fetched meal list
+        addMealList.length === 0 &&
+        <Spinner 
+          animation="border" 
+          role="status"
+          style={{
+            maxWidth: "480px",
+          }}
+        />
+      }
+      <div id="button-container" className="mt-5 d-flex justify-content-between align-items-center">
+        <Button onClick={() => {handleNextSuggestionsClick(-1)}}>Previous suggestions</Button>
+        <Button onClick={() => {handleNextSuggestionsClick(1)}}>More suggestions</Button>
+      </div>
+    </div>
+  );
+}
+
+// pane for searching items
+const SearchPane = (props) => {
   // fetch options for search
   const loadSearchOptions = (input, callback) => {
     fetch(`http://localhost:9000/search?query=${input}`, {
@@ -59,39 +117,48 @@ const SuggestionModal = (props) => {
     });
   }
   
-  const handleItemSelected = (listGroupIndex) => {
-    const newSelectedMealStates = [...selectedMealStates];
-    setSelectedMealStates(newSelectedMealStates.map((item, index) => {
-      if (index === listGroupIndex) return true;
-      else return false;
-    }))
-  }
+  const onchangeSelect = (item) => {
+    props.setMealToAdd(item.value);
+  };
   
-  const handleNextSuggestionsClick = (move) => {
-    // len = 3, index = 2, move= +1: index=(index+1)%len: index=2+1=3%3=0
-    // len = 3, index = 0, move= -1: index=(index+1)%len: index=2+1=3%3=0
-    // if index out of bound then wrap around
-    let nextIndex = currentSuggestionsIndex+move;
-    if (nextIndex === -1) nextIndex = addMealList.length-1
-    else if (nextIndex === addMealList.length) nextIndex = 0;
-    setCurrentSuggestionsIndex(nextIndex);
-    setSelectedMealStates([false, false, false])
-    //setSelectedMealStates(Array(data[0].length).fill(false))
-  }
+  return (
+    <div>
+      <h4>Search for a meal</h4>
+      <AsyncSelect 
+        loadOptions={loadSearchOptions} 
+        onChange={onchangeSelect}
+        className="mt-5" 
+        theme={(theme) => ({
+          ...theme,
+          //borderRadius: 0,
+          colors: {
+            ...theme.colors,
+            primary25: "#DD5353",
+            primary: "#F50057",
+          },
+        })}
+        />
+    </div>
+  );
 
-  // add an item, can only be called when an item is selected
+}
+
+const SuggestionModal = (props) => {
+  // whether to show pane with suggestions or pane with search bar
+  const [currentPane, setCurrentPane] = useState("suggestion");
+  // int repersenting id of meal to add
+  const [mealToAdd, setMealToAdd] = useState(-1)
+  
   const handleAddItem = () => {
-    const currentSelectedIndex = selectedMealStates.indexOf(true);
-    const mealToAdd = addMealList[currentSuggestionsIndex][currentSelectedIndex];
     props.onHide();
     const currentDate = new Date(props.date);
+
     const body = {
         date: `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`,
         mealNum: 0,
-        recipeId: mealToAdd.id
+        recipeId: mealToAdd,
     };
-    //console.log(JSON.stringify(body));
-    
+
     fetch("http://localhost:9000/calendar/meals", {
       method: "POST",
       headers: {
@@ -121,41 +188,26 @@ const SuggestionModal = (props) => {
           Add a meal?
         </Modal.Title>
       </Modal.Header>
+
       <Modal.Body>
-        <h4>Suggested meals</h4>
-        {
-          // if we have fetched meal list
-          addMealList.length !== 0 &&
-          <ListGroup as="ol" numbered>
-            {
-              addMealList[currentSuggestionsIndex].map((item,index) => (
-                <ListGroup.Item key={index} as="li" active={selectedMealStates[index]} onClick={() => handleItemSelected(index)}> {item.name} </ListGroup.Item>
-              ))
-            }
-          </ListGroup>
+        <div id="button-container" className="mt-5 mb-5 d-flex justify-content-center align-items-center">
+          <Button onClick={() => {setCurrentPane("suggestion")}} disabled={currentPane === "suggestion"}>Suggestions</Button>
+          <Button onClick={() => {setCurrentPane("search")}} disabled={currentPane === "search"}>Search Meal</Button>
+        </div>
+        { currentPane === "suggestion" &&
+          <SuggestionPane setMealToAdd={setMealToAdd}/>
         }
-        {
-          // if we have fetched meal list
-          addMealList.length === 0 &&
-          <Spinner 
-            animation="border" 
-            role="status"
-            style={{
-              maxWidth: "480px",
-            }}
-          />
+        { currentPane === "search" &&
+          <SearchPane setMealToAdd={setMealToAdd}/>
         }
-      <div id="button-container" className="mt-5 d-flex justify-content-between align-items-center">
-        <Button onClick={() => {handleNextSuggestionsClick(-1)}}>Previous suggestions</Button>
-        <Button onClick={() => {handleNextSuggestionsClick(1)}}>More suggestions</Button>
-      </div>
-      <AsyncSelect loadOptions={loadSearchOptions} className="mt-5" />
       </Modal.Body>
+
       <Modal.Footer>
         <Button 
           onClick={() => {handleAddItem()}}
-          disabled={!selectedMealStates.includes(true)}
+          disabled={!mealToAdd === -1}
         > 
+          {/* disabled={!selectedMealStates.includes(true)} */}
           <i className="bi bi-plus fs-4"></i>
         </Button>
         {/* <Button onClick={props.onHide}>Close</Button> */}
