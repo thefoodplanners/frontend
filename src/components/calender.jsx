303a77dd4a84371710866dac075359664f754f17
Ethan Hullett <ethull@protonmail.com>
2023-04-05 06:33:50 +0100

    calorie total hovers

--- src/components/calender.jsx
+++ src/components/calender.jsx
@@ -263,7 +263,7 @@ const Calender = () => {
             {close => (
               <div>
               <div style={{ float: "left", borderRight: "1px solid gray", padding: "5px", width: "65%" }}>
-              <button onClick={close}> &times; </button>/
+              <button onClick={close}> &times; </button>
               <div> <b>Name:</b> {recipe.name} </div>
               <div> {recipe.mealType} </div>
               <div> {recipe.desc} </div>
@@ -349,12 +349,40 @@ const Calender = () => {
     return currentWeekMeals[day].reduce((sum, item) => sum + item.recipe.calories, 0);
   }
   
+  // get total macros for a day
+  const getMacroDayTotals = (day) => {
+    const macros = {
+      fats: 0,
+      proteins: 0,
+      carbohydrates: 0,
+    }
+    if (!fetchedMeals) return macros;
+    macros["fats"] = currentWeekMeals[day].reduce((sum, item) => sum + item.recipe.fats, 0);
+    macros["proteins"] = currentWeekMeals[day].reduce((sum, item) => sum + item.recipe.proteins, 0);
+    macros["carbohydrates"] = currentWeekMeals[day].reduce((sum, item) => sum + item.recipe.carbohydrates, 0);
+    return macros;
+  }
+  
   // get total calories for the week
   const getTotalWeekCalories = () => {
     // if we havent fetched meals yet return nothing
     if (!fetchedMeals) return 0;
     // if we have sum calories for all meals in the current day
     return currentWeekMeals.reduce((sum, day) => sum + day.reduce((sum, meal) => sum + meal.recipe.calories, 0), 0)
+  }
+
+  // get total macros for a week
+  const getMacroWeekTotals = () => {
+    const macros = {
+      fats: 0,
+      proteins: 0,
+      carbohydrates: 0,
+    }
+    if (!fetchedMeals) return macros;
+    macros["fats"] = currentWeekMeals.reduce((sum, day) => sum + day.reduce((sum, meal) => sum + meal.recipe.fats, 0), 0);
+    macros["proteins"] = currentWeekMeals.reduce((sum, day) => sum + day.reduce((sum, meal) => sum + meal.recipe.proteins, 0), 0);
+    macros["carbohydrates"] = currentWeekMeals.reduce((sum, day) => sum + day.reduce((sum, meal) => sum + meal.recipe.carbohydrates, 0), 0);
+    return macros;
   }
 
   // render the rows of the calender
@@ -413,6 +441,98 @@ const Calender = () => {
     });
   }
   
+  const renderDayTotalCaloriesPopup = (day) => {
+    const totalDiv = renderDayTotalCalories(day);
+    const macros = getMacroDayTotals(day);
+    return (
+      <Popup
+        trigger={totalDiv}
+        position={['right center', 'bottom center']}
+        on="hover"
+        mouseEnterDelay={500}
+        {...{ contentStyle, arrowStyle }}
+        keepTooltipInside="#root"
+        nested
+      >
+        <div> <span style={{color: "#ff944d"}}><b>Fats:</b></span> {macros.fats.toFixed(1)}g </div>
+        <div> <span style={{color: "#e60000"}}><b>Proteins:</b></span> {macros.proteins.toFixed(1)}g </div>
+        <div> <span style={{color: "#cc9900"}}><b>Carbohydrates:</b></span> {macros.carbohydrates.toFixed(1)}g </div>
+        <Popup
+          trigger={<a href="#">View More</a>}
+          on="click"
+          contentStyle={{ background: "var(--bs-body-bg)" }}
+          modal
+          nested
+        >
+          {close => (
+            <div>
+              <button onClick={close}> &times; </button>
+              <div 
+                className="d-flex justify-content-center align-items-center mb-3"
+              >
+                <div style={{maxWidth: "300px", maxHeight: "500px"}}>
+                  {getMacrosPieChartData(macros)}
+                </div>
+              </div>
+            </div>
+          )}
+        </Popup>
+      </Popup>
+    );
+  }
+  
+  const renderDayTotalCalories = (day) => (
+    <div className="text-center text-white fw-bold total-calories-div">
+      {getTotalDayCalories(day)} daily calories
+    </div>
+  );
+
+  const renderWeekTotalCaloriesPopup = () => {
+    const totalDiv = renderWeekTotalCalories();
+    const macros = getMacroWeekTotals();
+    return (
+      <Popup
+        trigger={totalDiv}
+        position={['right center', 'bottom center']}
+        on="hover"
+        mouseEnterDelay={500}
+        {...{ contentStyle, arrowStyle }}
+        keepTooltipInside="#root"
+        nested
+      >
+        <div> <span style={{color: "#ff944d"}}><b>Fats:</b></span> {macros.fats.toFixed(1)}g </div>
+        <div> <span style={{color: "#e60000"}}><b>Proteins:</b></span> {macros.proteins.toFixed(1)}g </div>
+        <div> <span style={{color: "#cc9900"}}><b>Carbohydrates:</b></span> {macros.carbohydrates.toFixed(1)}g </div>
+        <Popup
+          trigger={<a href="#">View More</a>}
+          on="click"
+          contentStyle={{ background: "var(--bs-body-bg)" }}
+          modal
+          nested
+        >
+          {close => (
+            <div>
+              <button onClick={close}> &times; </button>
+              <div 
+                className="d-flex justify-content-center align-items-center mb-3"
+              >
+                <div style={{maxWidth: "300px", maxHeight: "500px"}}>
+                  {getMacrosPieChartData(macros)}
+                </div>
+              </div>
+            </div>
+          )}
+        </Popup>
+      </Popup>
+    );
+  }
+  
+  const renderWeekTotalCalories = () => (
+    <div className="text-center text-white fw-bold total-calories-div"> 
+      {getTotalWeekCalories()} weekly calories
+    </div>
+  );
+  
   return (
     <Container fluid>
       {renderWeekChanger()}
@@ -423,13 +543,11 @@ const Calender = () => {
             // render calorie summaries
             [0,1,2,3,4,5,6].map((item,index)=>(
               <div key={index} className="col-md-1">
-                <div className="text-center text-white fw-bold total-calories-div">
-                  {getTotalDayCalories(index)} daily calories
-                </div>
+                {renderDayTotalCaloriesPopup(index)}
               </div>
             ))
         }
-      <div className="mt-3" />
+        <div className="mt-3" />
         {
             // render add buttons
             [0,1,2,3,4,5,6].map((item,index)=>(
@@ -443,11 +561,7 @@ const Calender = () => {
       </div>
       <div className="row gx-0 d-flex justify-content-center align-items-center mt-3">
         <div className="col-xs-12">
-          <div 
-            className="text-center text-white fw-bold total-calories-div"
-          > 
-            {getTotalWeekCalories()} weekly calories
-          </div>
+          {renderWeekTotalCaloriesPopup()}
         </div>
       </div>
       <SuggestionModal
